49f8cefe4d8332bc7f043f7279d86504f5d09341
Gregoire Malzac <gregoire.malzac@gmail.com>
2018-04-02 21:00:13 +0100

    typewriter + avatar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,23 @@ import { initUpdateNavbarOnScroll } from '../../assets/javascripts/components/na
 initUpdateNavbarOnScroll();
 import { initUpdateNavbarLinkOnScroll } from '../../assets/javascripts/components/navbar';
 initUpdateNavbarLinkOnScroll();
+
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString('Web Applications')
+    .pauseFor(1500)
+    .deleteAll()
+    .typeString('Web Interfaces')
+    .pauseFor(1500)
+    .deleteAll()
+    .typeString('Websites')
+    .pauseFor(1500)
+    .deleteAll()
+    .typeString('Landing Pages')
+    .pauseFor(1500)
+    .deleteAll()
+    .start();
