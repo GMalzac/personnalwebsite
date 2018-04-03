74e5660edd7cbf5256f99c88d1b74b36fd8795f3
Gregoire Malzac <gregoire.malzac@gmail.com>
2018-04-03 17:11:27 +0100

    heroku

--- app/assets/javascripts/components/navbar.js
+++ app/assets/javascripts/components/navbar.js
@@ -9,21 +9,21 @@ function initUpdateNavbarOnScroll() {
   });
 }
 
-// export { initUpdateNavbarOnScroll };
+export { initUpdateNavbarOnScroll };
 
-// function initUpdateNavbarLinkOnScroll() {
-//   const navbarLinks = document.querySelectorAll('p.navbar-wagon-link');
-//   window.addEventListener('scroll', () => {
-//     if (window.scrollY >= window.innerHeight) {
-//       navbarLinks.forEach((item) => {
-//         item.classList.add('navbar-wagon-white-link');
-//       });
-//     } else {
-//       navbarLinks.forEach((item) => {
-//         item.classList.remove('navbar-wagon-white-link');
-//       });
-//     }
-//   });
-// }
+function initUpdateNavbarLinkOnScroll() {
+  const navbarLinks = document.querySelectorAll('.navbar-wagon-link');
+  window.addEventListener('scroll', () => {
+    if (window.scrollY >= window.innerHeight) {
+      navbarLinks.forEach((item) => {
+        item.classList.add('navbar-wagon-white-link');
+      });
+    } else {
+      navbarLinks.forEach((item) => {
+        item.classList.remove('navbar-wagon-white-link');
+      });
+    }
+  });
+}
 
-// export { initUpdateNavbarLinkOnScroll };
+export { initUpdateNavbarLinkOnScroll };
